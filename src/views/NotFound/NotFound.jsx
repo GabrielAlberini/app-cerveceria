3f74ff011a14e0a5fea24c73990321dbfe6539f4
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-12-04 08:06:17 -0300

    Contenido a página 404

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,22 +1,10 @@
+import { Banner } from "../../components/Banner/Banner";
 import { Layout } from "../../components/Layout/Layout";
 
 const NotFound = () => {
   return (
     <Layout>
-      <div className="columns is-centered">
-        <div className="column is-half">
-          <section className="hero is-danger">
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title">Not Found</h1>
-                <h2 className="subtitle">
-                  Sorry, the page you are looking for does not exist.
-                </h2>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
+      <Banner title="404 - No se encuentra la página :(" />
     </Layout>
   );
 };
